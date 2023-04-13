ee54806b69825fdfdef32fa6fb68a2c5a4eb2468
Snazo09 <119118651+Snazo09@users.noreply.github.com>
2023-04-13 15:12:38 +0200

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,16 +1,37 @@
 #!/usr/bin/python3
-""" pascal triangle"""
+""" Module that defines the class Student
+"""
 
 
-def pascal_triangle(n=4500):
-    """print pascal"""
-    pascal = [[0]*i for i in range(1, n+1)]
-    cmpt = 0
-    for i in range(n):
-        pascal[i][0] = 1
-        pascal[i][-1] = 1
-        for j in range(0, i//2):
-            pascal[i][j+1] = pascal[i-1][j] + pascal[i-1][j+1]
-            pascal[i][i-j-1] = pascal[i-1][j] + pascal[i-1][j+1]
+class Student:
+    """ Class to create student instances """
 
-    return pascal
+    def __init__(self, first_name, last_name, age):
+        """ Special method to initialize """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Method that returns directory description """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            d_list = {}
+
+            for iatr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[iatr] == satr:
+                        d_list[satr] = obj[satr]
+            return d_list
+
+        return obj
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the Student instance """
+        for atr in json:
+            self.__dict__[atr] = json[atr]
