c85ab85d6f162c34a9f83b32085720c19334eedd
Snazo09 <119118651+Snazo09@users.noreply.github.com>
2023-05-18 22:13:56 +0200

    Update 7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+--  inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+-- cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+INSERT INTO first_table VALUES (89, "Holberton School");
