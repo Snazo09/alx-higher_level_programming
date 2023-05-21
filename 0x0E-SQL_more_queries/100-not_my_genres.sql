efaa9f9e439d53df771c53f62a274f5c61530c0c
Snazo09 <119118651+Snazo09@users.noreply.github.com>
2023-05-21 22:09:23 +0200

    Update 100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,13 +1,16 @@
--- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
-   -- Each record should display:
-      -- tv_shows.title
-   -- Results must be sorted in ascending order by the show title
-   -- The database name will be passed as an argument of the mysql command
-
-SELECT tv_shows.title FROM tv_shows
-WHERE tv_shows.id NOT IN (
-      SELECT tv_shows.id FROM tv_shows
-      JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-      JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
-      WHERE tv_genres.name = "Comedy" )
-ORDER BY tv_shows.title;
+-- list all genres not linked to the show Dexter
+SELECT tv_genres.name AS name
+FROM tv_genres
+LEFT JOIN
+(
+	SELECT tv_genres.id, name
+  FROM tv_genres
+	JOIN tv_show_genres
+	     ON tv_genres.id = tv_show_genres.genre_id
+	JOIN tv_shows
+	     ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_shows.title = "Dexter"
+	ORDER BY tv_genres.id
+) dexter_genres ON dexter_genres.id = tv_genres.id
+WHERE dexter_genres.id is NULL
+ORDER BY name;
