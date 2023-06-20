17205e5705b12c393bfde1c23b58ca2f4026f96f
Snazo09 <119118651+Snazo09@users.noreply.github.com>
2023-06-20 23:09:31 +0200

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-// It prints 3 lines
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
